f33933af1b86d90567c854864eefd3b84afc2556
LingLiangChung <liangjun487@gmail.com>
2022-01-14 16:09:39 +0800

    delete

--- jobnow_users/android/app/src/main/kotlin/com/example/jobnow_users/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jobnow_users
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- jobnow_version_1/android/app/src/main/kotlin/com/example/jobnow_version_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jobnow_version_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
